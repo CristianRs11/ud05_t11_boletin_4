95601eb15f5c7a425c63a77779a5c9017c9ebab2
CristianRs11 <crodriguescalvo@gmail.com>
2022-03-07 12:17:38 +0100

    exercicio13

--- ejercicio_13.php
+++ ejercicio_13.php
@@ -4,25 +4,47 @@
     </head>
     <body>
         <?php
+            session_start();
+            $_SESSION['pos'];
+            $_SESSION['neg'];
             if (isset($_REQUEST["reinicio"])) {
+                $_REQUEST["int"]= 0;
                 $intentos = 0;
-                $pos = 0;
-                $neg = 0;
+                $_REQUEST["num"];
+                $num;
+                $_SESSION['pos'] = 0;
+                $_SESSION['neg'] = 0;
             }
-            $intentos = intval(isset($_REQUEST["int"])?$_REQUEST["int"]+1:0);
-            if ($intentos>10){
-        ?>
+            $intentos = isset($_REQUEST["int"])?$_REQUEST["int"]+1:0;
+            if ($intentos < 11){
+                ?>
+                <form action="ejercicio_13.php" enctype="multipart/form-data" method="POST">
+                    <input type="number" name="num">
+                    <input type="hidden" name="int" value="<?php echo $intentos;?>">
+                    <input type="submit" value="Enviar">
+                </form>
+                <?php
+                $num=is_numeric($_REQUEST['num'])?intval($_REQUEST["num"]):"a";
+                if (is_numeric($num)){
+                    if ($num >= 0) {
+                        $_SESSION['pos'] ++;
+                        echo "pos";
+                    }
+                    elseif ($num < 0){
+                        $_SESSION['neg'] ++;
+                        echo "neg";
+                    }
+                }
+            }
+            else{
+            ?>
             <form action="ejercicio_13.php" enctype="multipart/form-data" method="POST">
                 <input type="submit" name="reinicio" value="Reiniciar">
             </form>
         <?php 
-            echo "Hai $pos números positivos <br>";
-            echo "Hai $neg números positivos <br>";
-            }
-            else{
-
+            echo "Hai ".$_SESSION['pos']." números positivos <br>";
+            echo "Hai ".$_SESSION['neg']." números negativos <br>";
             }
         ?>
-        </table>
     </body>
 </html>
