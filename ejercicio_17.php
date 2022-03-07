5231aa1d0827a7c47500587589bac38ec968adad
CristianRs11 <crodriguescalvo@gmail.com>
2022-03-07 13:12:53 +0100

    exercicio17

---
+++ ejercicio_17.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form action="ejercicio_17.php" enctype="multipart/form-data" method="post">
+            <label>Escribe o número: <input type="number" name="nu1" style="width: 50px;" /></lable>
+            <br>
+            <input type="submit" value="Submit"/>
+            <br>
+        </form>
+        <?php
+            $num1 = $_POST['nu1'];
+            $resu = 0;
+            if (is_numeric($num1)) {
+                for ($i=0; $i < 101; $i++) { 
+                    $novo = $num1 + $i;
+                    $resu = $resu + $novo;
+                }
+                echo "A suma do número $num1 máis os seus 100 números siguientes é $resu";
+            }
+        ?>
+    </body>
+</html>
